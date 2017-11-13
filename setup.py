012b166a85ec1787338fed5e667dd801abfe4258
Jonathan Dursi <jonathan@dursi.ca>
2017-11-13 18:00:36 -0500

    Remove no-longer-necessary dependency links

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("requirements.txt") as requirementsFile:
         pinnedVersion = line.split()[0]
         install_requires.append(pinnedVersion)
 
-dependency_links = ["git+git://github.com/ljdursi/ga4gh-schemas.git@genotypes#egg=ga4gh_schemas"]
+dependency_links = []
 
 try:
     with open("constraints.txt") as constraintsFile:
