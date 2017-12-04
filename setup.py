5cce138c75c47eae4315ffc0052ffc61dec5b170
Jonathan Dursi <jonathan@dursi.ca>
2017-12-04 12:33:41 -0500

    Restore peer network

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("requirements.txt") as requirementsFile:
         pinnedVersion = line.split()[0]
         install_requires.append(pinnedVersion)
 
-dependency_links = []
+dependency_links = ["git+git://github.com/ljdursi/ga4gh-schemas.git@genotypes#egg=ga4gh_schemas"]
 
 try:
     with open("constraints.txt") as constraintsFile:
